b8f74e9001fec16a375b3e1e5e95d09f85e05fa9
2spirit <2spirit@free.fr>
2023-10-06 13:11:43 +0200

    Use my own scripts for building

---
+++ build_los-20-2spirit.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+chmod a+x ~/bin/repo
+
+#echo "configure git" # - replace user name and email with your own ones
+#echo "configure git" # - replace user name and email with your own ones
+#git config --global user.name "Your Name"
+#git config --global user.email "yourmail@email.com"
+#git config --global color.ui true #this stops the repo init colorization prompt
+
+echo "create directories"
+mkdir -p ~/android/lineage 
+mkdir -p ~/android/ccache/lineage
+mkdir -p ~/android/lineage/.repo/local_manifests
+
+echo "initiate the LineageOS repo"
+cd ~/android/lineage
+repo init -u https://github.com/2spirit/android.git -b lineage-20.0 --git-lfs
+
+echo "clone local manifest"
+wget https://raw.githubusercontent.com/2spirit/manifests/main/sweet_los-20_2spirit_roomservice.xml -P ~/android/lineage/.repo/local_manifests/
+
+echo "sync the source"
+repo sync -j$(nproc --all)
+
+echo "Copy missing google lib for keyboard swipe"
+mkdir -p ~/android/lineage/out/target/product/sweet/product/lib64/
+wget https://raw.githubusercontent.com/2spirit/manifests/main/libjni_latinimegoogle.so -P ~/android/lineage/out/target/product/sweet/product/lib64/
+
+echo "enable ccache"
+export USE_CCACHE=1 && export CCACHE_DIR=~/android/ccache/lineage && export CCACHE_EXEC=/usr/bin/ccache && ccache -M 100G
+
+echo "enable MicroG"
+export WITH_GMS=true
+
+echo "setup build environment"
+source build/envsetup.sh
+croot
+
+echo "prepare sweet for building"
+lunch lineage_sweet-userdebug
+
+echo "start building"
+mka bacon -j$(nproc --all)
+
