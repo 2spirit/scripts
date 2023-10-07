64a9833b4980f1c94316602cf5e5dfb84ad3405b
2spirit <2spirit@free.fr>
2023-10-07 10:48:56 +0200

    Correct path for repo

--- build_los-20-2spirit.sh
+++ build_los-20-2spirit.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+echo "create directories"
+mkdir -p ~/bin
+mkdir -p ~/android/lineage 
+mkdir -p ~/android/ccache/lineage
+mkdir -p ~/android/lineage/.repo/local_manifests
+
+echo "download last repo version"
 curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
 
@@ -9,20 +16,15 @@ chmod a+x ~/bin/repo
 #git config --global user.email "yourmail@email.com"
 #git config --global color.ui true #this stops the repo init colorization prompt
 
-echo "create directories"
-mkdir -p ~/android/lineage 
-mkdir -p ~/android/ccache/lineage
-mkdir -p ~/android/lineage/.repo/local_manifests
-
 echo "initiate the LineageOS repo"
 cd ~/android/lineage
-repo init -u https://github.com/2spirit/android.git -b lineage-20.0 --git-lfs
+~/bin/repo init -u https://github.com/2spirit/android.git -b lineage-20.0 --git-lfs
 
 echo "clone local manifest"
 wget https://raw.githubusercontent.com/2spirit/manifests/main/sweet_los-20_2spirit_roomservice.xml -P ~/android/lineage/.repo/local_manifests/
 
 echo "sync the source"
-repo sync -j$(nproc --all)
+~/bin/repo sync -j$(nproc --all)
 
 echo "Copy missing google lib for keyboard swipe"
 mkdir -p ~/android/lineage/out/target/product/sweet/product/lib64/
