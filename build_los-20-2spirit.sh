19c3943beab8a1774ee10b1f62d4e0bc27e13796
2spirit <64271596+2spirit@users.noreply.github.com>
2023-10-12 14:40:32 +0200

    Update build_los-20-2spirit.sh

--- build_los-20-2spirit.sh
+++ build_los-20-2spirit.sh
@@ -22,20 +22,21 @@ git config --global color.ui true #this stops the repo init colorization prompt
 
 echo "initiate the LineageOS repo"
 cd ~/android/lineage
-~/bin/repo init -u https://github.com/2spirit/android.git -b lineage-20.0 --git-lfs
+~/bin/repo init -u https://github.com/2spirit/android.git -b lineage-20.0 --git-lfs --depth=1
 
 echo "clone local manifest"
-wget --timestamping https://raw.githubusercontent.com/2spirit/manifests/main/sweet_los-20_2spirit_roomservice.xml -O ~/android/lineage/.repo/local_manifests/sweet_los-20_2spirit_roomservice.xml
+wget https://raw.githubusercontent.com/2spirit/manifests/main/sweet_los-20_2spirit_roomservice.xml -O ~/android/lineage/.repo/local_manifests/sweet_los-20_2spirit_roomservice.xml
 
 echo "sync the source"
-~/bin/repo sync -j$(nproc --all)
+#~/bin/repo sync -j$(nproc --all)
+~/bin/repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
 echo "Copy missing google lib for keyboard swipe"
 mkdir -p ~/android/lineage/out/target/product/sweet/product/lib64/
-wget --timestamping https://raw.githubusercontent.com/2spirit/scripts/main/libjni_latinimegoogle.so -O ~/android/lineage/out/target/product/sweet/product/lib64/libjni_latinimegoogle.so
+wget https://raw.githubusercontent.com/2spirit/scripts/main/libjni_latinimegoogle.so -O ~/android/lineage/out/target/product/sweet/product/lib64/libjni_latinimegoogle.so
 
 echo "enable ccache"
-export USE_CCACHE=1 && export CCACHE_DIR=~/android/ccache/lineage && export CCACHE_EXEC=/usr/bin/ccache && ccache -M 100G
+export USE_CCACHE=1 && export CCACHE_DIR=~/android/ccache/lineage && export CCACHE_EXEC=/usr/bin/ccache && ccache -M 50G
 
 echo "enable MicroG"
 export WITH_GMS=true
