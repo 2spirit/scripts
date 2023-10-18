45d943d280f498a1366eff142051bfd55de08b53
2spirit <2spirit@free.fr>
2023-10-18 16:35:27 +0200

    update compilation script

--- build_los-20-2spirit.sh
+++ build_los-20-2spirit.sh
@@ -1,35 +1,41 @@
 #!/usr/bin/env bash
 
-echo "setup the build environment"
-wget https://raw.githubusercontent.com/2spirit/scripts/master/setup_build_envirionment-ubuntu.sh
-chmod +x setup_build_envirionment-ubuntu.sh
-./setup_build_envirionment-ubuntu.sh
+#echo "setup the build environment"
+#wget https://raw.githubusercontent.com/2spirit/scripts/master/setup_build_envirionment-ubuntu.sh
+#chmod +x setup_build_envirionment-ubuntu.sh
+#./setup_build_envirionment-ubuntu.sh
 #rm setup_build_envirionment-ubuntu.sh
 
 echo "create directories"
 mkdir -p ~/bin
 mkdir -p ~/android/lineage 
 mkdir -p ~/android/ccache/lineage
-mkdir -p ~/android/lineage/.repo/local_manifests
 
 echo "download last repo version"
 curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
 
+echo "setup git config"
 git config --global user.name "build"
-git config --global user.email "build@email.com"
-git config --global color.ui true #this stops the repo init colorization prompt
+git config --global user.email "build@example.com"
+git config --global color.ui true
+
+echo "go to the working directory"
+cd ~/android/lineage
 
 echo "initiate the LineageOS repo"
-cd ~/android/lineage
-~/bin/repo init -u https://github.com/2spirit/android.git -b lineage-20.0 --git-lfs --depth=1
+~/bin/repo init -u https://github.com/2spirit/android.git -b lineage-20.0 --git-lfs
 
 echo "clone local manifest"
+mkdir -p ~/android/lineage/.repo/local_manifests
 wget https://raw.githubusercontent.com/2spirit/manifests/main/sweet_los-20_2spirit_roomservice.xml -O ~/android/lineage/.repo/local_manifests/sweet_los-20_2spirit_roomservice.xml
 
 echo "sync the source"
 #~/bin/repo sync -j$(nproc --all)
 ~/bin/repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+
+echo "Add Leica Camera"
+git clone https://gitlab.com/unsatifsed27/miuicamera.git -b leica vendor/xiaomi/sweet-miuicamera
 
 echo "Copy missing google lib for keyboard swipe"
 mkdir -p ~/android/lineage/out/target/product/sweet/product/lib64/
